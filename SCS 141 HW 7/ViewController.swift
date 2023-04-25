bd7ea98487534d7ae64629d1c96b836d2e35f0b2
Austin Patton <SCS374Group1@gmail.com>
2023-04-25 16:49:35 -0400

    assignment is complete

--- SCS 141 HW 7/ViewController.swift
+++ SCS 141 HW 7/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var zoomImage: UILabel!
+    
+    @IBAction func zoomAction(_ sender: Any) {
+        zoomImage.text = "The frog is now bigger!"
+    }
+    
+    @IBOutlet weak var tapImage: UILabel!
+    @IBAction func tapAction(_ sender: Any) {
+        tapImage.text = "Button tapped!"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
